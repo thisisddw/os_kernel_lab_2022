29e6b9a194416e0b0d282e1d128e006a1a67d70a
ddw <thisisddw@gmail.com>
2022-10-15 21:34:47 +0800

    little tune

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -140,7 +140,6 @@ print_regs(struct pushregs *regs) {
     cprintf("  eax  0x%08x\n", regs->reg_eax);
 }
 
-struct trapframe switchk2u, *switchu2k;
 /* trap_dispatch - dispatch based on what type of trap occurred */
 static void
 trap_dispatch(struct trapframe *tf) {
